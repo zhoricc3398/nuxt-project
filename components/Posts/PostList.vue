e62732fa59eb0e1feae2e3ee0658740338db6674
g.sharabidze <g.sharabidze@vendoo.ge>
2019-10-22 12:01:24 +0400

    New Project New Life

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hello there - the second time!"
+      previewText="This is my second post!"
+    />
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+      title="Hi!"
+      previewText="This is my third post!"
+    />
+  </section>
+</template>
+
+<script>
+  import PostPreview from '@/components/Posts/PostPreview'
+
+  export default {
+    components: {
+      PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
