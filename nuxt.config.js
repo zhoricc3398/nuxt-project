635209853b12a5adc2d31857a075e8fe91b26ab8
g.sharabidze <g.sharabidze@vendoo.ge>
2019-10-24 12:53:12 +0400

    feat: the Loading Property

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#fa923f', height: '4px', duration: 5000 },
   /*
   ** Global CSS
   */
